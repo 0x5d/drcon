ac80e8d629012d908478ed30b4035ffdd1b91307
David Castillo <castillobgr@gmail.com>
2015-11-12 19:12:19 -0500

    Added a surprise...

--- src/github.com/castillobg/example/server/main.go
+++ src/github.com/castillobg/example/server/main.go
@@ -5,11 +5,21 @@ import (
   "net/http"
 )
 
+func johnnyHandler(w http.ResponseWriter, r *http.Request) {
+  html := `<html>
+    </body>
+      <img src="https://media.giphy.com/media/RoajqIorBfSE/giphy.gif"></img>
+    </body>
+    </html>`
+  fmt.Fprintf(w, "%s", html)
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
   fmt.Fprintf(w, "Hello, it's me: %s", r.URL.Path[1:])
 }
 
 func main() {
+  http.HandleFunc("/johnny", johnnyHandler)
   http.HandleFunc("/", handler)
   http.ListenAndServe(":8080", nil)
 }
